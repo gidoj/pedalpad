39317670b3473e5b889a0fa86e853c359f68a663
Joseph Gido <jgido1@verizon.net>
2023-07-26 20:57:00 -0400

    Update pedalpad_custom.js

--- pedalpad_custom.js
+++ pedalpad_custom.js
@@ -1,1 +1,12 @@
+window.ec = window.ec || Object();
+window.ec.config = window.ec.config || Object();
+window.ec.config.chameleon = window.ec.config.chameleon || Object();
+window.ec.config.chameleon.colors = {
+  'color-background':'#D3D3D3',
+  'color-foreground':'#4EA3F0',
+  'color-link':'#FF0606',
+  'color-button':'#4EA3F0',
+  'color-price':'#FF0606'
+}
 
+Ecwid.refreshConfig();
